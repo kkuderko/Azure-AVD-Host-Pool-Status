6fd7c01dc11ffde3dbdbf94b056c84eb3d0db80a
kkuderko <kkuderko@users.noreply.github.com>
2022-05-20 12:46:58 +0100

    Update AzureAVDHostPoolStatus-cmd.ps1

--- AzureAVDHostPoolStatus-cmd.ps1
+++ AzureAVDHostPoolStatus-cmd.ps1
@@ -10,6 +10,7 @@ run the script
 #>
 param ($HostPool,$ResourceGroup)
 $AvailableHosts = 0
+$DrainModeHosts = 0
 
 $AVDHostList = Get-AzWvdSessionHost -ResourceGroupName $ResourceGroup -HostPoolName $HostPool
 $TotalAVDHosts = ($AVDHostList).count
@@ -17,20 +18,30 @@ $TotalAVDSessions = ($AVDHostList | Select-Object Session | Measure-Object -Prop
 $HostPoolSessionLimit = (Get-AzWvdHostPool -ResourceGroupName $ResourceGroup -Name $HostPool).MaxSessionLimit
 
 foreach ($AVDHost in $AVDHostList){
-	if ($AVDHost.Status -eq "Available" -And $AVDHost.AllowNewSession -eq "True") {
+	if ($AVDHost.Status -like "Available" -And $AVDHost.AllowNewSession -like "True") {
 		$AvailableHosts ++
+	}
+	if ($AVDHost.Status -like "Available" -And $AVDHost.AllowNewSession -like "False") {
+		$DrainModeHosts ++
 	}
 }
 
 if ($AvailableHosts -eq 0){
-	$HColour = "DarkRed"
+	$AHColour = "DarkRed"
 } else {
-	$HColour = "DarkGreen"
+	$AHColour = "DarkGreen"
 }
+
+if ($DrainModeHosts -eq 0){
+	$DHColour = "DarkGreen"
+} else {
+	$DHColour = "DarkYellow"
+}
+
 $CurrentSessionLimit = $HostPoolSessionLimit * $AvailableHosts
 $SessionCapacityP = [math]::ceiling(($TotalAVDSessions*100)/($CurrentSessionLimit))
 
-$Users = Get-AzWvdUserSession -ResourceGroupName $ResourceGroup -HostPoolName $HostPool | Select-Object UserPrincipalName, Name
+$Users = Get-AzWvdUserSession -ResourceGroupName $ResourceGroup -HostPoolName $HostPool | Select-Object UserPrincipalName, Name, SessionState
 
 Clear-Host
 Write-Host "AVD Host Pool Name:" $HostPool -ForegroundColor Blue
@@ -38,5 +49,6 @@ Write-Host ($Users | Format-Table -AutoSize | Out-String) -NoNewLine -Foreground
 Write-Host "AVD Sessions Usage: $($TotalAVDSessions) out of $($CurrentSessionLimit) ($($SessionCapacityP)%)" -ForegroundColor Yellow
 
 Write-Host ($AVDHostList | Select-Object Name,Session,AllowNewSession,Status | Format-Table -AutoSize | Out-String) -NoNewLine -ForegroundColor DarkGray
-Write-Host "AVD Hosts:" $AvailableHosts "available out of" $TotalAVDHosts -ForegroundColor $HColour
+Write-Host "AVD Hosts:" $AvailableHosts "available out of" $TotalAVDHosts -ForegroundColor $AHColour
+Write-Host "Hosts in drain mode:" $DrainModeHosts -ForegroundColor $DHColour
 Write-Host
