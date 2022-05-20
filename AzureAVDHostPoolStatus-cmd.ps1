73d5d5f1700cd4c3baed3af94839716bcd1e8fd8
kkuderko <kkuderko@users.noreply.github.com>
2022-05-20 12:12:37 +0100

    Update AzureAVDHostPoolStatus-cmd.ps1

--- AzureAVDHostPoolStatus-cmd.ps1
+++ AzureAVDHostPoolStatus-cmd.ps1
@@ -17,7 +17,7 @@ $TotalAVDSessions = ($AVDHostList | Select-Object Session | Measure-Object -Prop
 $HostPoolSessionLimit = (Get-AzWvdHostPool -ResourceGroupName $ResourceGroup -Name $HostPool).MaxSessionLimit
 
 foreach ($AVDHost in $AVDHostList){
-	if ($AVDHost.Status -eq "Available") {
+	if ($AVDHost.Status -eq "Available" -And $AVDHost.AllowNewSession -eq "True") {
 		$AvailableHosts ++
 	}
 }
@@ -37,6 +37,6 @@ Write-Host "AVD Host Pool Name:" $HostPool -ForegroundColor Blue
 Write-Host ($Users | Format-Table -AutoSize | Out-String) -NoNewLine -ForegroundColor DarkGray
 Write-Host "AVD Sessions Usage: $($TotalAVDSessions) out of $($CurrentSessionLimit) ($($SessionCapacityP)%)" -ForegroundColor Yellow
 
-Write-Host ($AVDHostList | Select-Object Name,Session,Status | Format-Table -AutoSize | Out-String) -NoNewLine -ForegroundColor DarkGray
+Write-Host ($AVDHostList | Select-Object Name,Session,AllowNewSession,Status | Format-Table -AutoSize | Out-String) -NoNewLine -ForegroundColor DarkGray
 Write-Host "AVD Hosts:" $AvailableHosts "available out of" $TotalAVDHosts -ForegroundColor $HColour
 Write-Host
