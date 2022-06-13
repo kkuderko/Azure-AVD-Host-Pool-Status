b6716e1fb4aad3b6a07019f52276a066e8c60042
kkuderko <kkuderko@users.noreply.github.com>
2022-06-13 09:18:00 +0100

    Update AzureAVDHostPoolStatus-cmd.ps1

--- AzureAVDHostPoolStatus-cmd.ps1
+++ AzureAVDHostPoolStatus-cmd.ps1
@@ -41,14 +41,19 @@ if ($DrainModeHosts -eq 0){
 $CurrentSessionLimit = $HostPoolSessionLimit * $AvailableHosts
 $SessionCapacityP = [math]::ceiling(($TotalAVDSessions*100)/($CurrentSessionLimit))
 
-$Users = Get-AzWvdUserSession -ResourceGroupName $ResourceGroup -HostPoolName $HostPool | Select-Object UserPrincipalName, Name, SessionState
+$Users = Get-AzWvdUserSession -ResourceGroupName $ResourceGroup -HostPoolName $HostPool
+
+$ActiveUsers = ($Users | Select-Object SessionState | select-string -pattern "Active").count
+$DisconnectedUsers = ($Users | Select-Object SessionState | select-string -pattern "Disconnected").count
 
 Clear-Host
 Write-Host "AVD Host Pool Name:" $HostPool -ForegroundColor Blue
-Write-Host ($Users | Format-Table -AutoSize | Out-String) -NoNewLine -ForegroundColor DarkGray
-Write-Host "AVD Sessions Usage: $($TotalAVDSessions) out of $($CurrentSessionLimit) ($($SessionCapacityP)%)" -ForegroundColor Yellow
-
+Write-Host ($Users | Select-Object UserPrincipalName, Name, SessionState | Format-Table -AutoSize | Out-String) -NoNewLine -ForegroundColor DarkGray
 Write-Host ($AVDHostList | Select-Object Name,Session,AllowNewSession,Status | Format-Table -AutoSize | Out-String) -NoNewLine -ForegroundColor DarkGray
-Write-Host "AVD Hosts:" $AvailableHosts "available out of" $TotalAVDHosts -ForegroundColor $AHColour
-Write-Host "Hosts in drain mode:" $DrainModeHosts -ForegroundColor $DHColour
+Write-Host "AVD Sessions Usage:`t$($TotalAVDSessions) out of $($CurrentSessionLimit) ($($SessionCapacityP)%)" -ForegroundColor Yellow
+Write-Host "Active Users:`t`t$($ActiveUsers)" -ForegroundColor Yellow
+Write-Host "Disconnected Users:`t$($DisconnectedUsers)" -ForegroundColor Yellow
+Write-Host
+Write-Host "AVD Hosts:`t`t" $AvailableHosts "available out of" $TotalAVDHosts -ForegroundColor $AHColour
+Write-Host "Hosts in drain mode:`t" $DrainModeHosts -ForegroundColor $DHColour
 Write-Host
